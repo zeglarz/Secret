a0e57b114be699eeddfeaaa334321f26a79b0818
root <zeglarz@gmail.com>
2020-01-08 22:40:02 +0000

    add md5 encyption

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
-
+const md5 = require('md5');
 const app = express();
 
 app.use(express.static('public'));
@@ -20,8 +19,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema)
 
@@ -45,7 +42,7 @@ app.post('/register', (req, res) => {
   const newUser = new User(
     {
       email: req.body.username,
-      password: req.body.password
+      password: md5(req.body.password)
     });
     newUser.save(err => {
       if (err) {
@@ -58,7 +55,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req,res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
     if (err) {
